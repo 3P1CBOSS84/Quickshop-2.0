a1249eaac564867eaaa6732b9a6ed3e879ae4909
3P1CBOSS84 <michaelaaronboulter@yahoo.com>
2022-07-02 21:04:45 -0400

    Initial Commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("QuickShop")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("QuickShop")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: CompilationRelaxations(8)]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | DebuggableAttribute.DebuggingModes.EnableEditAndContinue)]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ea2657af-8211-4322-ac6c-a537f9366fd6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ QuickShop.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using Harmony12;
+using System;
+using System.Reflection;
+using UnityModManagerNet;
+
+namespace QuickShop
+{
+    internal static class Main
+    {
+        private static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            Main.mod = modEntry;
+            modEntry.OnToggle = new Func<UnityModManager.ModEntry, bool, bool>(Main.OnToggle);
+            HarmonyInstance harmonyInstance = HarmonyInstance.Create(modEntry.Info.Id);
+            harmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+            return true;
+        }
+
+        private static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            Main.enabled = value;
+            modEntry.Logger.Log("Starting QuickShop");
+            return true;
+        }
+        public static bool enabled;
+
+        public static UnityModManager.ModEntry mod;
+    }
+
+    [HarmonyPatch(typeof(GameScript))]
+    [HarmonyPatch("Update")]
+    public class RotationInput_Update_Patcher
+    {
+        [HarmonyPostfix]
+        private static void Postfix()
+        {
+            bool keyUp = Input.GetKeyUp(KeyCode.B);
+            if (keyUp)
+            {
+                GameScript.Get().GetSelectedPartToMount();
+                string idwithTuned = GameScript.Get().GetPartMouseOver().GetIDWithTuned();
+                idwithTuned.Replace("1", "2");
+                bool flag = idwithTuned != null;
+                if (flag)
+                {
+                    Main.mod.Logger.Log(idwithTuned);
+                    Inventory.Get().Add(idwithTuned, 1f, Color.black, true, true);
+                    new NewInventoryItem(idwithTuned, true).Condition = 1f;
+                    int price = Singleton<GameInventory>.Instance.GetItemProperty(idwithTuned).Price;
+                    GlobalData.AddPlayerMoney(-price);
+                    UIManager.Get().ShowPopup("QuickShop Mod:", "Part cost: " + Helper.MoneyToString((float)price), PopupType.Buy);
+                }
+            }
+
+            if (Input.GetKeyUp(KeyCode.N))
+            {
+                GameScript.Get().GetSelectedPartToMount();
+                string idwithTuned2 = "t_" + GameScript.Get().GetPartMouseOver().GetIDWithTuned();
+                idwithTuned2.Replace("1", "2");
+                bool flag = idwithTuned2 != null;
+                if (flag)
+                {
+                    Main.mod.Logger.Log(idwithTuned2);
+                    Inventory.Get().Add(idwithTuned2, 1f, Color.black, true, true);
+                    new NewInventoryItem(idwithTuned2, true).Condition = 1f;
+                    int price = Singleton<GameInventory>.Instance.GetItemProperty(idwithTuned2).Price;
+                    GlobalData.AddPlayerMoney(-price);
+                    UIManager.Get().ShowPopup("QuickShop Mod:", "Part cost: " + Helper.MoneyToString((float)price), PopupType.Buy);
+                }
+            }
+        }
+    }
+}
